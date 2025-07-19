ffd9361daceeb693f38480f30c86b1e13f27b20d
Matthew Seah <matscottseah@gmail.com>
2025-07-19 16:14:26 -0400

    Initial Commit

---
+++ Stroll/StrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrollApp.swift
+//  Stroll
+//
+//  Created by Matthew Seah on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct StrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StrollTests/StrollTests.swift
@@ -0,0 +1,16 @@
+//
+//  StrollTests.swift
+//  StrollTests
+//
+//  Created by Matthew Seah on 7/19/25.
+//
+
+import Testing
+
+struct StrollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
